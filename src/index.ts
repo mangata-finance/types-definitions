cedc1d90995a9758e2824ebe926c459aca46600e
Michal Danco <devdanco@gmail.com>
2023-07-03 14:49:22 +0200

    Rc/1.0.0 (#1)
    * ci: add workflows
    
    * ci: add semantic release
    
    * chore(release): 1.0.0-rc.1 [skip ci]
    
    # 1.0.0-rc.1 (2023-06-30)
    
    ### Features
    
    * add type definition for mangata ([2e3fcce](https://github.com/mangata-finance/types-definitions/commit/2e3fcce90dad378bb692cea1d57cf2594d1d81cf))
    
    ### BREAKING CHANGES
    
    * split types pacakge into two packages
    
    * feat: split types and rpc into separate variables
    
    * chore(release): 1.0.0-rc.2 [skip ci]
    
    # [1.0.0-rc.2](https://github.com/mangata-finance/types-definitions/compare/v1.0.0-rc.1...v1.0.0-rc.2) (2023-06-30)
    
    ### Features
    
    * split types and rpc into separate variables ([766a4c3](https://github.com/mangata-finance/types-definitions/commit/766a4c333f3cd65ef0b0284bd1cf6906ae238add))
    
    * fix: node version 16
    
    * chore: semantic version change
    
    * chore(release): 1.0.0-rc.3 [skip ci]
    
    # [1.0.0-rc.3](https://github.com/mangata-finance/types-definitions/compare/v1.0.0-rc.2...v1.0.0-rc.3) (2023-06-30)
    
    ### Bug Fixes
    
    * node version 16 ([acb55a6](https://github.com/mangata-finance/types-definitions/commit/acb55a6bb9bef6f99ab3a8e1da747d971c0ea6f3))
    
    * feat: add descriptions
    
    * chore(release): 1.0.0-rc.4 [skip ci]
    
    # [1.0.0-rc.4](https://github.com/mangata-finance/types-definitions/compare/v1.0.0-rc.3...v1.0.0-rc.4) (2023-07-03)
    
    ### Features
    
    * add descriptions ([d0754cd](https://github.com/mangata-finance/types-definitions/commit/d0754cdaa80d575ea418ac24738a0fba3a54fc20))
    
    * ci: update workflow
    
    ---------
    
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>

--- src/index.ts
+++ src/index.ts
@@ -1,235 +1,246 @@
 import { OverrideBundleDefinition } from "@polkadot/types/types";
 
+export const mTypes = {
+  ShufflingSeed: {
+    seed: "H256",
+    proof: "H512"
+  },
+  Header: {
+    parentHash: "Hash",
+    number: "Compact<BlockNumber>",
+    stateRoot: "Hash",
+    extrinsicsRoot: "Hash",
+    digest: "Digest",
+    seed: "ShufflingSeed",
+    count: "BlockNumber"
+  },
+  XYKRpcResult: {
+    price: "Balance"
+  },
+  RPCAmountsResult: {
+    firstAssetAmount: "Balance",
+    secondAssetAmount: "Balance"
+  },
+  VestingInfo: {
+    locked: "Balance",
+    perBlock: "Balance",
+    startingBlock: "BlockNumber"
+  },
+  TokenId: "u32",
+  VestingInfosWithLockedAt: {
+    vestingInfosWithLockedAt:
+      "Vec<(VestingInfo<Balance, BlockNumber>, Balance)>"
+  }
+};
+
+export const mRpc = {
+  xyk: {
+    calculate_buy_price: {
+      description:
+        "Calculates and returns sold_token_amount while providing bought_token_amount and respective reserves",
+      params: [
+        {
+          name: "input_reserve",
+          type: "Balance"
+        },
+        {
+          name: "output_reserve",
+          type: "Balance"
+        },
+        {
+          name: "sell_amount",
+          type: "Balance"
+        }
+      ],
+      type: "XYKRpcResult<Balance>"
+    },
+    calculate_sell_price: {
+      description:
+        "Calculates and returns bought_token_amount while providing sold_token_amount and respective reserves",
+      params: [
+        {
+          name: "input_reserve",
+          type: "Balance"
+        },
+        {
+          name: "output_reserve",
+          type: "Balance"
+        },
+        {
+          name: "sell_amount",
+          type: "Balance"
+        }
+      ],
+      type: "XYKRpcResult<Balance>"
+    },
+    get_burn_amount: {
+      description:
+        "Returns amounts of tokens received by burning provided liquidity_token_amount in pool of provided token ids",
+      params: [
+        {
+          name: "first_asset_id",
+          type: "TokenId"
+        },
+        {
+          name: "second_asset_id",
+          type: "TokenId"
+        },
+        {
+          name: "liquidity_asset_amount",
+          type: "Balance"
+        }
+      ],
+      type: "RPCAmountsResult<Balance>"
+    },
+    calculate_sell_price_id: {
+      description:
+        "Same as calculate_sell_price, but providing token_id instead of reserves. Reserves are fetched by function.",
+      params: [
+        {
+          name: "sold_token_id",
+          type: "TokenId"
+        },
+        {
+          name: "bought_token_id",
+          type: "TokenId"
+        },
+        {
+          name: "sell_amount",
+          type: "Balance"
+        }
+      ],
+      type: "XYKRpcResult<Balance>"
+    },
+    calculate_buy_price_id: {
+      description:
+        "Same as calculate_buy_price, but providing token_id instead of reserves. Reserves are fetched by function.",
+      params: [
+        {
+          name: "sold_token_id",
+          type: "TokenId"
+        },
+        {
+          name: "bought_token_id",
+          type: "TokenId"
+        },
+        {
+          name: "buy_amount",
+          type: "Balance"
+        }
+      ],
+      type: "XYKRpcResult<Balance>"
+    },
+    calculate_rewards_amount: {
+      description:
+        "Calculate rewards amount of liquidity token id for the user",
+      params: [
+        {
+          name: "user",
+          type: "AccountId"
+        },
+        {
+          name: "liquidity_asset_id",
+          type: "TokenId"
+        }
+      ],
+      type: "XYKRpcResult<Balance>"
+    },
+    calculate_balanced_sell_amount: {
+      description:
+        "Calculates how much amount x we need to swap from total_amount, so that after y = swap(x), the resulting balance equals (total_amount - x) / y = pool_x / pool_y - the resulting amounts can then be used to `mint_liquidity` with minimal leftover after operation",
+      params: [
+        {
+          name: "total_amount",
+          type: "Balance"
+        },
+        {
+          name: "reserve_amount",
+          type: "Balance"
+        }
+      ],
+      type: "XYKRpcResult<Balance>"
+    },
+    get_max_instant_unreserve_amount: {
+      description: "",
+      params: [
+        {
+          name: "user",
+          type: "AccountId"
+        },
+        {
+          name: "liquidity_asset_id",
+          type: "TokenId"
+        }
+      ],
+      type: "XYKRpcResult<Balance>"
+    },
+    get_max_instant_burn_amount: {
+      description: "",
+      params: [
+        {
+          name: "user",
+          type: "AccountId"
+        },
+        {
+          name: "liquidity_asset_id",
+          type: "TokenId"
+        }
+      ],
+      type: "XYKRpcResult<Balance>"
+    },
+    is_sell_asset_lock_free: {
+      description: "",
+      params: [
+        {
+          name: "path",
+          type: "Vec<TokenId>"
+        },
+        {
+          name: "input_amount",
+          type: "Balance"
+        }
+      ],
+      type: "Option<bool>"
+    },
+    is_buy_asset_lock_free: {
+      description: "",
+      params: [
+        {
+          name: "path",
+          type: "Vec<TokenId>"
+        },
+        {
+          name: "input_amount",
+          type: "Balance"
+        }
+      ],
+      type: "Option<bool>"
+    }
+  },
+  vesting: {
+    getVestingLockedAt: {
+      description: "",
+      params: [
+        {
+          name: "who",
+          type: "AccountId"
+        },
+        {
+          name: "token_id",
+          type: "TokenId"
+        }
+      ],
+      type: "VestingInfosWithLockedAt<Balance, BlockNumber>"
+    }
+  }
+};
+
 export const mangataTypesBundleForPolkadotApps: OverrideBundleDefinition = {
   types: [
     {
       minmax: [0, undefined],
-      types: {
-        ShufflingSeed: {
-          seed: "H256",
-          proof: "H512"
-        },
-        Header: {
-          parentHash: "Hash",
-          number: "Compact<BlockNumber>",
-          stateRoot: "Hash",
-          extrinsicsRoot: "Hash",
-          digest: "Digest",
-          seed: "ShufflingSeed",
-          count: "BlockNumber"
-        },
-        XYKRpcResult: {
-          price: "Balance"
-        },
-        RPCAmountsResult: {
-          firstAssetAmount: "Balance",
-          secondAssetAmount: "Balance"
-        },
-        VestingInfo: {
-          locked: "Balance",
-          perBlock: "Balance",
-          startingBlock: "BlockNumber"
-        },
-        TokenId: "u32",
-        VestingInfosWithLockedAt: {
-          vestingInfosWithLockedAt:
-            "Vec<(VestingInfo<Balance, BlockNumber>, Balance)>"
-        }
-      }
+      types: mTypes
     }
   ],
-  rpc: {
-    xyk: {
-      calculate_buy_price: {
-        description: "",
-        params: [
-          {
-            name: "input_reserve",
-            type: "Balance"
-          },
-          {
-            name: "output_reserve",
-            type: "Balance"
-          },
-          {
-            name: "sell_amount",
-            type: "Balance"
-          }
-        ],
-        type: "XYKRpcResult<Balance>"
-      },
-      calculate_sell_price: {
-        description: "",
-        params: [
-          {
-            name: "input_reserve",
-            type: "Balance"
-          },
-          {
-            name: "output_reserve",
-            type: "Balance"
-          },
-          {
-            name: "sell_amount",
-            type: "Balance"
-          }
-        ],
-        type: "XYKRpcResult<Balance>"
-      },
-      get_burn_amount: {
-        description: "",
-        params: [
-          {
-            name: "first_asset_id",
-            type: "TokenId"
-          },
-          {
-            name: "second_asset_id",
-            type: "TokenId"
-          },
-          {
-            name: "liquidity_asset_amount",
-            type: "Balance"
-          }
-        ],
-        type: "RPCAmountsResult<Balance>"
-      },
-      calculate_sell_price_id: {
-        description: "",
-        params: [
-          {
-            name: "sold_token_id",
-            type: "TokenId"
-          },
-          {
-            name: "bought_token_id",
-            type: "TokenId"
-          },
-          {
-            name: "sell_amount",
-            type: "Balance"
-          }
-        ],
-        type: "XYKRpcResult<Balance>"
-      },
-      calculate_buy_price_id: {
-        description: "",
-        params: [
-          {
-            name: "sold_token_id",
-            type: "TokenId"
-          },
-          {
-            name: "bought_token_id",
-            type: "TokenId"
-          },
-          {
-            name: "buy_amount",
-            type: "Balance"
-          }
-        ],
-        type: "XYKRpcResult<Balance>"
-      },
-      calculate_rewards_amount: {
-        description: "",
-        params: [
-          {
-            name: "user",
-            type: "AccountId"
-          },
-          {
-            name: "liquidity_asset_id",
-            type: "TokenId"
-          }
-        ],
-        type: "XYKRpcResult<Balance>"
-      },
-      calculate_balanced_sell_amount: {
-        description: "",
-        params: [
-          {
-            name: "total_amount",
-            type: "Balance"
-          },
-          {
-            name: "reserve_amount",
-            type: "Balance"
-          }
-        ],
-        type: "XYKRpcResult<Balance>"
-      },
-      get_max_instant_unreserve_amount: {
-        description: "",
-        params: [
-          {
-            name: "user",
-            type: "AccountId"
-          },
-          {
-            name: "liquidity_asset_id",
-            type: "TokenId"
-          }
-        ],
-        type: "Balance"
-      },
-      get_max_instant_burn_amount: {
-        description: "",
-        params: [
-          {
-            name: "user",
-            type: "AccountId"
-          },
-          {
-            name: "liquidity_asset_id",
-            type: "TokenId"
-          }
-        ],
-        type: "Balance"
-      },
-      is_sell_asset_lock_free: {
-        description: "",
-        params: [
-          {
-            name: "path",
-            type: "Vec<TokenId>"
-          },
-          {
-            name: "input_amount",
-            type: "Balance"
-          }
-        ],
-        type: "bool"
-      },
-      is_buy_asset_lock_free: {
-        description: "",
-        params: [
-          {
-            name: "path",
-            type: "Vec<TokenId>"
-          },
-          {
-            name: "input_amount",
-            type: "Balance"
-          }
-        ],
-        type: "bool"
-      }
-    },
-    vesting: {
-      getVestingLockedAt: {
-        description: "",
-        params: [
-          {
-            name: "who",
-            type: "AccountId"
-          },
-          {
-            name: "token_id",
-            type: "TokenId"
-          }
-        ],
-        type: "VestingInfosWithLockedAt<Balance, BlockNumber>"
-      }
-    }
-  }
+  rpc: mRpc
 };
